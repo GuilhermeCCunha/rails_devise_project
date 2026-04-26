7ab3378fc20c471f1520c54d1009a3828fd94166
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2026-04-26 15:17:27 -0300

    generating views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
-  get 'pages/home'
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions',
+    registrations: 'admin/registrations'
+  }
+
+  devise_for :users, controllers: {
+    sessions:  'users/sessions'  
+  }
+        
+  root 'pages#home'
   get 'pages/users'
-  get 'pages/admin'
+  post 'pages/ban/:id', to: 'pages#ban_user', as: 'pages_ban_user'
+  post 'pages/unban/:id', to: 'pages#unban_user', as: 'pages_unban_user'
+  get 'pages/admin', to: 'pages#admin', as: 'pages_admin'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+ 
