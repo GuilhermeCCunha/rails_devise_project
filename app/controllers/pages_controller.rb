ca3558279993e4d62adfa92de8b84a3368e22ee6
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2026-04-30 16:07:07 -0300

    admin page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:users], unless: :admin_signed_in?
+  before_action :authenticate_admin!, only: [:admin]
+
   def home
   end
 
@@ -6,5 +9,18 @@ class PagesController < ApplicationController
   end
 
   def admin
+    @users = User.all
+  end
+
+  def ban_user
+    user = User.find(params[:id])
+    user.lock_access!
+    redirect_to pages_admin_path
+  end
+
+  def unban_user
+    user = User.find(params[:id])
+    user.unlock_access!
+    redirect_to pages_admin_path
   end
 end
