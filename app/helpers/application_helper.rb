0319ff9b2991124150f8aa63e116706f50988899
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2026-07-09 15:59:08 -0300

    add nav_link_to helper and improve navbar accessibility

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def nav_link_to(text, path)
+    is_active = current_page?(path)
+    css_class = "nav-link #{'active' if is_active}"
+    aria_attrs = is_active ? { current: 'page' } : {}
+    link_to text, path, class: css_class, aria: aria_attrs
+  end
 end
