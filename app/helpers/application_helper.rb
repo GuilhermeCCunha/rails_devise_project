c988fa8d7706898cee8bbdadf744db4699c42bdf
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2026-07-25 22:06:44 -0300

    add locale_link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
     aria_attrs = is_active ? { current: 'page' } : {}
     link_to text, path, class: css_class, aria: aria_attrs
   end
+
+  def locale_link(locale)
+    is_active = I18n.locale.to_s == locale.to_s
+    css_class = "dropdown-item #{'active' if is_active}"
+    link_to(
+      t('locale_name', locale: locale),
+      root_path(locale: locale),
+      class: css_class
+    )
+  end
 end
