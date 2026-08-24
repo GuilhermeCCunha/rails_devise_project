b923ebb1401dd81f32d86d5100b5376618718aad
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2026-08-24 13:54:44 -0300

    translate max admin registration limit alert message

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -5,7 +5,7 @@ class Admins::RegistrationsController < Devise::RegistrationsController
 
   def check_admin_limit
     if Admin.count >= Admin::MAX_ADMINS
-      redirect_to new_admin_session_path, alert: "The maximum number of admins has been reached."
+      redirect_to new_admin_session_path, alert: t('.limit_reached', resource: 'admins')
     end
   end
 end
