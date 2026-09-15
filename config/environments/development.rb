b4d06e81afae22495c73b1e0d5e73d2b95d5652f
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2026-09-15 19:59:03 -0300

    docs: add comment for mailer default port and remove old configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,9 +36,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -71,6 +68,7 @@ Rails.application.configure do
   }
 
   # Set default URL options for email links
+  # This should be the port where your Rails app is running (e.g., 3000, 3001, or 3002).
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Raises error for missing translations.
