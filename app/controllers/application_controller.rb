d31d7a5bf906dca12a08d2281979a8896f531fef
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2026-09-17 22:39:27 -0300

    fix: prevent setting invalid languages in i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,30 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    if params[:locale]
-      cookies[:locale] = params[:locale]
+    requested_locale = params[:locale]&.downcase
+
+    if requested_locale.present? && locale_valid?(requested_locale)
+      matched_locale = I18n.available_locales.find { |l| l.to_s.downcase == requested_locale }
+      cookies[:locale] = matched_locale.to_s
     end
 
-    if cookies[:locale]
-      if I18n.locale != cookies[:locale]
-        I18n.locale = cookies[:locale]
-      end
+    I18n.locale = extract_locale
+  end
+
+  private
+
+  def extract_locale
+    current_cookie = cookies[:locale]&.downcase
+
+    if current_cookie.present? && locale_valid?(current_cookie)
+      I18n.available_locales.find { |l| l.to_s.downcase == current_cookie }
+    else
+      I18n.default_locale
     end
+  end
+
+  def locale_valid?(locale)
+    supported_locales = I18n.available_locales.map { |l| l.to_s.downcase }
+    supported_locales.include?(locale.to_s.downcase)
   end
 end
